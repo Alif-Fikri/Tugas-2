bb4c38d319a1d8f89ea69fb22af3f5a565703798
Alif-Fikri <aliefmaulanafikri@gmail.com>
2024-04-16 22:47:47 +0700

    first commit

---
+++ app/src/main/java/com/example/tugas2/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.tugas2;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.tugas2.R;
+
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void loginClicked(View view) {
+        Intent loginClicked = new Intent(MainActivity.this, login.class);
+        startActivity(loginClicked);
+    }
+}
